7276a8bf3ab9e09f1014c50f59206e9d46a52db1
Benny Capozzo <bennycapozzo@outook.com>
2022-03-25 14:23:13 +0100

    esercizio 3

---
+++ esercizio3/src/app/models/models.ts
@@ -0,0 +1,42 @@
+export interface User {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: Address;
+  role: Role;
+  username: string;
+  profilePhotoUrl: string;
+  gender: Gender;
+}
+
+interface Address {
+  city: string;
+  street: string;
+  postalCode: string;
+}
+
+interface Companies {
+  id: number;
+  name: string;
+  description: string;
+  location: {
+    city: string;
+    street: string;
+    postalCode: string;
+  };
+}
+
+export enum Gender {
+  FEMALE = 'female',
+  MALE = 'male',
+  OTHER = 'other',
+}
+
+export enum Role {
+  STAFF = 'staff',
+  STUDENT = 'student',
+  MANAGER = 'manager',
+  ADMIN = 'admin',
+}
